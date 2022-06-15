38dab31383b19c35c22ecd8125e80891a040f870
Yoonjin Jung <37509450+citrusj@users.noreply.github.com>
2022-06-15 17:50:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-    
-const octokit = github.getOctokit(token);
-const response = await octokit.pulls.update(request);
 
-try {
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
+async function run() {
+    try {
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
 
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+        const payload = JSON.stringify(github.context.payload, undefined, 2);
+        console.log(`The event payload: ${payload}`);
 
-    const request = {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: github.context.payload.pull_request.number,
-    }
-
-    const token = core.getInput('repo-token')
-    const pullRequestTitle = core.getInput('pr-title')
-    const pullRequestNumber = github.context.payload.pull_request.number
-
-    console.log(pullRequestTitle)
-    console.log(pullRequestNumber)
-
-    if (!pullRequestTitle.toString().includes('[')) {
-        let updatedTitle = `[${pullRequestNumber}] ` + pullRequestTitle;
-        core.setOutput('titleUpdated', updatedTitle)
-
-        console.log(updatedTitle)
-
-        request.title = updatedTitle;
-
-
-        if (response.status !== 200) {
-            core.error("failed");
+        const request = {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: github.context.payload.pull_request.number,
         }
-        console.log("Title Updated !")
+
+        const token = core.getInput('repo-token')
+        const pullRequestTitle = core.getInput('pr-title')
+        const pullRequestNumber = github.context.payload.pull_request.number
+
+        console.log(pullRequestTitle)
+        console.log(pullRequestNumber)
+
+        const octokit = github.getOctokit(token);
+        const response = await octokit.pulls.update(request);
+        
+        if (!pullRequestTitle.toString().includes('[')) {
+            let updatedTitle = `[${pullRequestNumber}] ` + pullRequestTitle;
+            core.setOutput('titleUpdated', updatedTitle)
+
+            console.log(updatedTitle)
+
+            request.title = updatedTitle;
+
+
+            if (response.status !== 200) {
+                core.error("failed");
+            }
+            console.log("Title Updated !")
+        }
+    } catch (error) {
+        core.setFailed(error.message);
     }
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+run()
