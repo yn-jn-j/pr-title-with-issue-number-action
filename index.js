6397837727a1ed055877c0553f4599ecc818d4bc
yoonjin.jung <yoonjin.jung@navercorp.com>
2022-06-15 17:36:16 +0900

    test PR title change

--- index.js
+++ index.js
@@ -10,6 +10,36 @@ try {
 
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
+
+    const request = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: github.context.payload.pull_request.number,
+    }
+
+    const token = core.getInput('repo-token')
+    const pullRequestTitle = core.getInput('pr-title')
+    const pullRequestNumber = github.context.payload.pull_request.number
+
+    console.log(pullRequestTitle)
+    console.log(pullRequestNumber)
+
+    if (!pullRequestTitle.toString().includes('[')) {
+        let updatedTitle = `[${pullRequestNumber}] ` + pullRequestTitle;
+        core.setOutput('titleUpdated', updatedTitle)
+
+        console.log(updatedTitle)
+
+        request.title = updatedTitle;
+
+        const octokit = github.getOctokit(token);
+        const response = await octokit.pulls.update(request);
+
+        if (response.status !== 200) {
+            core.error("failed");
+        }
+        console.log("Title Updated !")
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
